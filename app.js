3a85faf21cddb3e3fea41c71cbfe207ec4c28cca
Olugbenga Raymond <olugbengaraymond20@gmail.com>
2023-12-30 14:07:44 +0100

    Creating a city entry page

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express'); // Imports the express module
 const https = require('https'); // Imports the https module
+const bodyParser = require('body-parser'); // Imports the body-parser module
 
 const app = express(); // Initializes the express instance
 
-app.get('/', (req, res) => {
+app.use(bodyParser.urlencoded({extented: true}));
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?appid=33fe1fb24000457d83c9f587d21300ce&q=tokyo&units=metric";
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post("/", (req,res) => {
+    const city = req.body.cityName;
+    const APIKey = '33fe1fb24000457d83c9f587d21300ce';
+    const units = 'metric';
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?appid=${APIKey}&q=${city}&units=${units}`;
 
     https.get(url, function(response){
         console.log(response.statusCode);
@@ -16,15 +26,12 @@ app.get('/', (req, res) => {
             const icon = weatherData.weather[0].icon;
             const weatherDescription = weatherData.weather[0].description;
             const iconURL = `https://openweathermap.org/img/wn/${icon}@2x.png`;
-            res.write(`<h1>The weather in Lagos is ${temp} <sup>o</sup>C, ${weatherDescription}</h1>`);
+            res.write(`<h1>The weather in ${city} is ${temp} <sup>o</sup>C, ${weatherDescription}</h1>`);
             res.write(`<img src='${iconURL}'/>`);
             res.send();
         })
     });
-    // res.send("Welcome");
 });
-
-
 
 app.listen(3000, function() {
     console.log('listening on port 3000');
